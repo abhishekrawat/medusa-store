b7bf44ef152bae41d41f8bdda98ae6e78e3d45c2
Abhishek Rawat <hi@rawat.dev>
2022-07-04 17:19:05 +0400

    initial

--- medusa-config.js
+++ medusa-config.js
@@ -32,15 +32,15 @@ const plugins = [
 
 module.exports = {
   projectConfig: {
-    redis_url: REDIS_URL,
+    // redis_url: REDIS_URL,
+    // For more production-like environment install PostgresQL
+    // database_url: DATABASE_URL,
+    // database_type: "postgres",
+    // database_database: "postgres://rawat:Reset@4207@localhost:5432/rawat",
     database_url: DATABASE_URL,
-    database_type: "postgres",
+    atabase_type: "postgres",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
-    database_extra:
-      process.env.NODE_ENV !== "development"
-        ? { ssl: { rejectUnauthorized: false } }
-        : {},
   },
   plugins,
 };
