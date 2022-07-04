b215467b86c012fa6e9dbf7123e9a0f43d7a408b
Abhishek Rawat <hi@rawat.dev>
2022-07-04 09:19:47 +0400

    Deploy medusa server on heroku

--- medusa-config.js
+++ medusa-config.js
@@ -32,14 +32,15 @@ const plugins = [
 
 module.exports = {
   projectConfig: {
-    // redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
-    // database_url: DATABASE_URL,
+    redis_url: REDIS_URL,
+    database_url: DATABASE_URL,
     database_type: "postgres",
-    database_database: "postgres://postgres:Reset@4207@localhost:5432/medusa",
-    // database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
+    database_extra:
+      process.env.NODE_ENV !== "development"
+        ? { ssl: { rejectUnauthorized: false } }
+        : {},
   },
   plugins,
 };
